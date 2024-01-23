61d8872c3a44784c4c58ac75b340b27c666c4ddb
Denis Ahn <denis.ahn@yandex.ru>
2024-01-24 00:46:10 +0300

    feat Release

--- src/pages/AppRoutes/MainRoutes/Demo/MOCK.js
+++ src/pages/AppRoutes/MainRoutes/Demo/MOCK.js
@@ -11,4 +11,4 @@ export const MOCK = [
     [false, false, false, false, false, false, true, false, true, false],
     [false, false, false, false, false, true, false, true, false, true],
     [true, true, true, true, true, true, true, true, true, true],
-]
+];
